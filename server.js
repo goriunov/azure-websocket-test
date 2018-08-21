e62fefef5efabf91d03c0118169d32c7a999371d
Dmitrii Goriunov <goriunovd@gmail.com>
2018-08-21 23:00:51 +1200

    Minor improvments

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ let server = new uws.WebSocketServer({ port: process.env.PORT || 3000 }, () => {
 });
 
 server.on('connection', (socket) => {
+    console.log('Server is connected');
     socket.on('message', (message) => {
         socket.send(message);
     });
