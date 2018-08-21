4091817077e9077302c41c1508edb6daab57fec2
Dmitrii Goriunov <goriunovd@gmail.com>
2018-08-21 22:35:37 +1200

    Added new console

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const uws = require('clusterws-uws');
 
-let server = new uws.WebSocketServer({ port: process.env.PORT || 3000 }, () => console.log("Server is running on: 3000"));
+let server = new uws.WebSocketServer({ port: process.env.PORT || 3000 }, () => {
+    console.log(`${process.platform} _ ${process.versions.modules}`)
+    console.log("Server is running on: 3000")
+});
 
 server.on('connection', (socket) => {
     socket.on('message', (message) => {
