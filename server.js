d5f6518e8163da0f370a40370c0f20a99539d49a
Dmitrii Goriunov <goriunovd@gmail.com>
2018-08-21 23:04:30 +1200

    Switch connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const uws = require('clusterws-uws');
+const uws = require('uws');
 
-let server = new uws.WebSocketServer({ port: process.env.PORT || 3000 }, () => {
+let server = new uws.Server({ port: process.env.PORT || 3000 }, () => {
     console.log(`${process.platform} _ ${process.versions.modules}`)
     console.log("Server is running on: 3000")
 });
